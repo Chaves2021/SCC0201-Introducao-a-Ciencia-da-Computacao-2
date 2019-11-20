d7b3ab3ef6b14b935026239af267062bae48a2e6
Viana2021 <caiomarcoschaves@gmail.com>
2019-11-20 14:56:16 -0300

    Atualizando

--- Algoritmos-Ordenacao/shell_sort/shell_sort.c
+++ Algoritmos-Ordenacao/shell_sort/shell_sort.c
@@ -4,11 +4,37 @@
  INSERTION SORT IMPROVED
  INSERTION SORT MOVES ONLY ADJACENT ELEMENTS
  SHELL SORT MOVES DISTANT ELEMENTS YAY :P
- ELEMENTS SEPARATED BY H POSITIONS (MULTIPLES OF H), FOR EXAMPLE H = 4
+ ELEMENTS SEPARATED BY H POSITIONS (MULTIPLES OF H), FOR EXAMPLE H = 2
 
- [H*0]   [1]	[2]	[3]	[H*1]
-  10      3      5       2        1
+ [H*0]   [1]	[H*1]	[3]
+  10      3      5       2
 
- AFTER SORTING
- [H*0]   [1]	[2]	[3]	[H*1]
-   1      3      5       2        10
+ SORTING: STEP 1
+ [H*0]   [1]	[H*1]	 [3]
+   5      3      10       2
+
+ SORTING: STEP 2
+  [0]   [H*0]	[2]	 [H*1]
+   5      2      10        3
+
+ AFTER THAT, DECREASE H
+
+ AFTER H == 1, DO INSERTION SORT
+
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define SUCCESS		0
+#define TRUE		1
+#define FALSE		0
+
+int main(void){
+	
+
+
+
+
+	return SUCCESS;
+}
